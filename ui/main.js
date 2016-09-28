89cd21129cff99b98e3e184aa0886c409f844a8f
anjali3112soni <anjali3112soni@users.noreply.github.com>
2016-09-28 12:50:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
 console.log('Loaded!');
+
+
+var text= document.getElementById("text");
+text.innerHtml="Hi i am anjali";
+
+var madi =document.getElementById("madi");
+
+var leftmargin=0;
+function moveright()
+{
+    leftmargin= leftmargin+10;
+    madi.style.margineleft= leftmargin + "px";
+}
+
+madi.onclick= function(){
+  var interval =setInterval(moveright,100);  
+};
