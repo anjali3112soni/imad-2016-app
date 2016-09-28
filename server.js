f5d9e49e795ae1450d5eae773a5ef24adcbc7322
anjali3112soni <anjali3112soni@users.noreply.github.com>
2016-09-28 10:53:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/action-one', function(req,res){
-    res.send("This is my action one");
+     res.sendFile(path.join(__dirname, 'ui', 'action-one'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
