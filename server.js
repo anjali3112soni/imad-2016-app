3af001b65feeed8418658cf007669054f80eff7b
anjali3112soni <anjali3112soni@users.noreply.github.com>
2016-11-10 00:49:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config = {
              database :'anjali3112soni' ,
              host :'db.imad.hasura-app.io',
              port:'5432',
-             password:process.env.DB_PASSWORD
+             password: process.env.DB_PASSWORD
              };
 
 app.get('/', function (req, res) {
