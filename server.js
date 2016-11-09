e9e46d8a7c126dfd620c9c94d125a13f3d19b48d
anjali3112soni <anjali3112soni@users.noreply.github.com>
2016-11-09 11:15:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,12 +16,17 @@ app.get('/ui/style.css', function (req, res) {
      res.sendFile(path.join(__dirname, 'ui', 'action-one'));
 });*/
 app.get('/ui/p5.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', '007.gif'));
+  res.sendFile(path.join(__dirname, 'ui', 'p5.jpg'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
+app.get('/ui/git.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'git.png'));
+});
+app.get('/ui/facebook.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'facebook.png'));
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
